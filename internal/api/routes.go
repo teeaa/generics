4d087b8e057c041f96ae5c6514fab79a4c5be44a
Teea Alarto <theteea@gmail.com>
2024-11-10 15:25:32 +0100

    first

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -5,20 +5,17 @@ import (
 
 	"github.com/go-fuego/fuego"
 	"github.com/teeaa/generics/internal/models"
-	"github.com/teeaa/generics/internal/service"
 )
 
 func (a *Api) setRoutes() {
 	v1 := fuego.Group(a.api, "/v1")
 	tx := fuego.Group(v1, "/tx/{txid}")
 
-	setItemRoutes[models.Address](tx, a.service, "address")
-	setItemRoutes[models.Dob](tx, a.service, "dob")
-	setItemRoutes[models.Name](tx, a.service, "name")
+	setItemRoutes[models.Address](tx, "address")
+	setItemRoutes[models.Dob](tx, "dob")
+	setItemRoutes[models.Name](tx, "name")
 }
 
-func setItemRoutes[T models.DataType](route *fuego.Server, srv *service.Service, itemType string) {
+func setItemRoutes[T models.DataType](route *fuego.Server, itemType string) {
 	fuego.Post(route, fmt.Sprintf("/%s", itemType), Create[T])
-	// fuego.Post(route, fmt.Sprintf("/%s", itemType), NewOps[T](srv, itemType).Create)
-	// fuego.Get(route, fmt.Sprintf("/%s", itemType), NewOps[T](srv, itemType).Get)
 }
